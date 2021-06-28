597da347ca2e51b98850012628c4cbaf2addeff7
Haneen M Alhajji ahmed <85841915+Haneen-Alhajjiahmed@users.noreply.github.com>
2021-06-28 18:47:19 +0300

    Delete connection.php

--- connection.php
+++
@@ -1,7 +0,0 @@
-<?php  
-       $con = mysqli_connect("localhost" , "root" , "" , "robotArmControl");
-       if(mysqli_connect_errno()){
-           mysqli_connect_error();
-           exit();
-       }
-?> 
